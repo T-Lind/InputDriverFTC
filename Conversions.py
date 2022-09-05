9d6a6088ea88eae0ee905f68b7fbc885f11d67ee
T-Lind <tiernanxklindauer@gmail.com>
2022-09-05 10:52:11 -0500

    convert

--- Conversions.py
+++ Conversions.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 from FieldDisplay import FieldDisplay
 from FieldDisplayRobot import Robot
@@ -13,7 +14,7 @@ class Conversions:
         self.back_right = []
         self.timing = []
 
-    def one_vel(self, robot: Robot):
+    def one_vel(self, robot: Robot, run_time):
         angle = math.atan2(robot.vel_x, robot.vel_y)
         magnitude = math.hypot(robot.vel_x, robot.vel_y)
 
@@ -47,9 +48,28 @@ class Conversions:
         self.back_left.append(bl)
         self.front_right.append(fr)
         self.back_right.append(br)
+        self.timing.append(int((time.time()-run_time)))
 
     def __call__(self, field_display: FieldDisplay):
         while not field_display.finished:
-            self.one_vel(field_display.robot_kinematics)
-
+            self.one_vel(field_display.robot_kinematics, field_display.run_time)
             field_display()
+
+        output_file_fl = 'fl.data'
+        output_file_bl = 'bl.data'
+        output_file_fr = 'fr.data'
+        output_file_br = 'br.data'
+        fw_fl = open(output_file_fl, 'wb')
+        fw_bl = open(output_file_bl, 'wb')
+        fw_fr = open(output_file_fr, 'wb')
+        fw_br = open(output_file_br, 'wb')
+
+        pickle.dump(self.front_left, fw_fl)
+        pickle.dump(self.back_left, fw_bl)
+        pickle.dump(self.front_right, fw_fr)
+        pickle.dump(self.back_right, fw_br)
+
+        fw_fl.close()
+        fw_bl.close()
+        fw_fr.close()
+        fw_br.close()
