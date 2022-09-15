da303f7bb91747b649bc770a6066d1337241878e
T-Lind <tiernanxklindauer@gmail.com>
2022-09-15 13:37:03 -0500

    following correct auto path to park in zone three

--- InputDriverFTC/main.py
+++ InputDriverFTC/main.py
@@ -11,7 +11,15 @@ robot = FieldDisplay(ROBOT_INIT_X=3.35, ROBOT_INIT_Y=0.88, ROBOT_INIT_HEADING=18
 def get_x(t):
     if t < 2:
         return 0.64 * math.cos(math.pi / 2 * t) - 0.64
-    return -0.64 * 2
+    if t < 4.4:
+        return -0.64 * 2
+    if t < 4.9:
+        return -0.085 * math.cos(2 * math.pi * t + 0.63) - 0.085 + -0.64 * 2
+    if t < 25:
+        return -0.64 * 2 - 0.085 * 2
+    if t < 25.5:
+        return (-0.64 * 2 - 0.085 * 2) - 0.1 * math.cos(2 * math.pi * t) + 0.1
+    return -0.64 * 2 + 0.05
 
 
 def get_y(t):
@@ -19,15 +27,21 @@ def get_y(t):
         return 0
     if t < 3.4:
         return -0.21 * math.sin(0.7 * math.pi * t - 0.9) + 0.105
-    return 0.044
+    if t < 25.25:
+        return 0.044
+    if t < 26.25:
+        return 0.044-0.3-0.3*math.cos(math.pi*t-0.79)
+    return -0.556
+
+
 
 
 def get_heading(t):
     if t < 3.4:
         return 0
-    elif t < 4.4:
-        return -45 * math.cos(0.75 * t * math.pi - 1.7) + 45
-    return 90
+    if t < 4.4:
+        return -51 * math.cos(math.pi * t - 1.25) - 51
+    return -102
 
 
 if __name__ == "__main__":
